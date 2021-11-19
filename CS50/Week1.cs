7904e1d8e8d06e0a73ee8f7a612acbc23e267eff
Jack <jackanthonys@hotmail.com>
2021-11-19 07:04:05 -0500

    Created project and completed Mario

---
+++ CS50/Week1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS50
+{
+    class Week1
+    {
+        public static void Pryamid()
+        {
+            Console.WriteLine("Choose a height between 1-20");
+            Console.Write("Height: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            for (int i = 1; i <= h; i++)
+            {
+                string pyramid = new string('#', i);
+                string space = new string(' ', h - i);
+                Console.WriteLine(space + pyramid + "  " + pyramid);
+            }
+        }
+    }
+}
